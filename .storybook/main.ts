6ec5a6246d71c35610ec0865f306bead069bed45
minsu <>
2025-11-05 20:24:13 +0900

    mod: 스토리북 세팅 수정, 예제파일 삭제

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,36 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import viteConfig from '../vite.config.ts';
+import react from '@vitejs/plugin-react-swc';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  async viteFinal(config) {
+    // 기존 vite 설정과 병합
+    return mergeConfig(config, {
+      ...viteConfig,
+      plugins: [react()],
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+          },
+        },
+      },
+    });
+  },
 };
+
 export default config;
